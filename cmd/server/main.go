33b426a183f7fac298315e8d67cf6631d5e81c12
Pabis21 <pg24abf@herts.ac.uk>
2026-03-08 17:46:35 +0000

    buffer added

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"log"
 	"net/http"
@@ -10,11 +11,21 @@ const targetServer = "https://jsonplaceholder.typicode.com"
 
 func proxyHandler(w http.ResponseWriter, r *http.Request) {
 
+	// Read request body
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Restore body so it can be used again
+	r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+
 	// Build new request to real server
 	req, err := http.NewRequest(
 		r.Method,
 		targetServer+r.URL.Path,
-		r.Body,
+		bytes.NewBuffer(bodyBytes),
 	)
 
 	if err != nil {
@@ -22,17 +33,22 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	req.Header = r.Header
+	// Copy headers
+	req.Header = r.Header.Clone()
 
+	// HTTP client
 	client := &http.Client{}
 
+	// Send request
 	resp, err := client.Do(req)
-
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadGateway)
 		return
 	}
-
+	log.Println("Captured Request:")
+	log.Println("Method:", r.Method)
+	log.Println("Path:", r.URL.Path)
+	log.Println("Body:", string(bodyBytes))
 	defer resp.Body.Close()
 
 	// Copy response headers
@@ -42,8 +58,10 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	// Write response status
 	w.WriteHeader(resp.StatusCode)
 
+	// Copy response body
 	io.Copy(w, resp.Body)
 }
 
